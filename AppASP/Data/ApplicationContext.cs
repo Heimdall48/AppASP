83ca59991ce45cd9e719b0eca425d3b534401556
Александр Нелюбин <alexnelub@yandex.ru>
2022-10-19 12:17:20 +0300

    Добавление редактирование модели

--- AppASP/Data/ApplicationContext.cs
+++ AppASP/Data/ApplicationContext.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.SqlClient;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace AppASP.Data
 {
@@ -26,9 +27,34 @@ namespace AppASP.Data
         public object GetModelImage(int pModel_ID)
         {
             Microsoft.Data.SqlClient.SqlParameter vModel_ID = new Microsoft.Data.SqlClient.SqlParameter("@Model_ID", pModel_ID);
-            SqlParameter vPhoto = new SqlParameter("@Photo", System.Data.SqlDbType.Binary, 8000, System.Data.ParameterDirection.Output, true, 0, 0, string.Empty, System.Data.DataRowVersion.Default, DBNull.Value);
+            SqlParameter vPhoto = new SqlParameter("@Photo", System.Data.SqlDbType.NVarChar, 3000000, System.Data.ParameterDirection.Output, true, 0, 0, string.Empty, System.Data.DataRowVersion.Default, DBNull.Value);
             this.Database.ExecuteSqlRaw("SELECT TOP 1 @Photo = M.Photo FROM dbo.Model M WHERE M.Model_ID = @Model_ID", vPhoto, vModel_ID);
             return vPhoto.Value;
+        }
+
+        public void SaveModel(string name, int current_ID, string description, string? image, AppASP.Models.ItemModify itemmodify)
+        {
+            var vName = new SqlParameter("@Name", name);
+            var vDescription = new SqlParameter("@Description", string.IsNullOrWhiteSpace(description) ? DBNull.Value : description);
+            var vPhoto = new SqlParameter("@Photo", string.IsNullOrWhiteSpace(image) ? DBNull.Value : image);
+
+            if (current_ID == 0)
+            {
+                SqlParameter vParameter = new SqlParameter("@Model_ID", System.Data.SqlDbType.Int, 8, System.Data.ParameterDirection.Output, true, 0, 0, string.Empty, System.Data.DataRowVersion.Default, DBNull.Value);
+                this.Database.ExecuteSqlRaw(@"INSERT INTO [dbo].[Model] ([Name],[Photo] ,[Description]) 
+                                              VALUES (@Name,@Photo, @Description)
+                                              SELECT @Model_ID = SCOPE_IDENTITY()", vName, vPhoto, vDescription, vParameter);
+                itemmodify.ID = Convert.ToInt32(vParameter.Value);
+            }
+            else
+            {
+                var vModel_ID = new SqlParameter("@Model_ID", current_ID);
+
+                itemmodify.ID = current_ID;
+                this.Database.ExecuteSqlRaw(@"UPDATE [dbo].[Model]
+                                              SET [Name] = @Name, [Photo] = @Photo, [Description] = @Description
+                                              WHERE Model_ID = @Model_ID", vName, vPhoto, vDescription, vModel_ID);
+            }
         }
     }
   
---
+++ AppASP/Models/ItemModify.cs
@@ -0,0 +1,13 @@
+﻿namespace AppASP.Models
+{
+    public class ItemModify
+    {
+        public bool IsExists { get; set; }
+        public int? ID { get; set; }
+        public ItemModify()
+        {
+            IsExists = true;
+            ID = null;
+        }
+    }
+}
